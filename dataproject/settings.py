3a25614f6cb470707d0654858717c0cc0ec44691
Valentine <valentine@mail.com>
2026-02-26 05:17:49 -0800

    install gunicorn and set allowed hosts

--- dataproject/settings.py
+++ dataproject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.getenv('DEBUG')) == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'data-entry-app-au0d.onrender.com']
 
 CACHES = {
     "default": {
